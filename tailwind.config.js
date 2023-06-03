799d5567edcd126016ce000bcba5ea9de5b4345b
Mehdiyunis <mehdiyunis1110@gmail.com>
2023-06-03 19:28:39 +0400

    header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./public/*.{html,js}"],
   theme: {
-    extend: {},
+    container:{
+      center: true,
+      screens:{
+        lg:'1140px',
+        xl:'1140px',
+        '2xl':'1140px',
+      }
+    },
+    extend: {
+      fontFamily :{
+        gemunu:[ 'Gemunu Libre', 'sans-serif'],
+        open:['Open Sans', 'sans-serif']
+      },
+      colors:{
+        'blue':'rgb(212, 212, 212)',
+        'light-yellow':'rgb(255, 253, 110)',
+        'light-red':'rgb(255, 93, 93)'
+      }
+    },
   },
   plugins: [],
 }
